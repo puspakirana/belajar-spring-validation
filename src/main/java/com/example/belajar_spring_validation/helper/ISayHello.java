de7c058f83df529a017ae704343d81d02de761e8
puspakirana <puspakiranastv@gmail.com>
2025-03-30 19:15:31 +0700

    Method Overriding

---
+++ src/main/java/com/example/belajar_spring_validation/helper/ISayHello.java
@@ -0,0 +1,9 @@
+package com.example.belajar_spring_validation.helper;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public interface ISayHello {
+    String sayHello(@NotBlank String name);
+}
--- src/main/java/com/example/belajar_spring_validation/helper/SayHello.java
+++ src/main/java/com/example/belajar_spring_validation/helper/SayHello.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 @Component
-@Validated
-public class SayHello {
+//@Validated
+public class SayHello implements ISayHello {
     public String sayHello(@NotBlank String name) {
         return "Hello " + name;
     }
