3b511143612d514fe544bddbc6b68ab24fbe932a
puspakirana <puspakiranastv@gmail.com>
2025-03-30 19:30:55 +0700

    Configuration Properties

--- src/main/java/com/example/belajar_spring_validation/BelajarSpringValidationApplication.java
+++ src/main/java/com/example/belajar_spring_validation/BelajarSpringValidationApplication.java
@@ -1,9 +1,14 @@
 package com.example.belajar_spring_validation;
 
+import com.example.belajar_spring_validation.configuration.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		DatabaseProperties.class
+})
 public class BelajarSpringValidationApplication {
 
 	public static void main(String[] args) {
